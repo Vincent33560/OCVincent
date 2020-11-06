e51c2c58fd2d29d442dd64c2c7e0c63fb470a1cb
Vincent33560 <qwezedits@hotmail.fr>
2020-11-06 23:33:49 +0100

    add int.py

---
+++ interfaces.py
@@ -0,0 +1,16 @@
+from main import connect
+
+
+
+def inte():
+    connection.send("conf t\n")
+    time.sleep(.5)
+    connection.send("int g0/1\n")
+    time.sleep(2)
+    connection.send("ip address 192.168.10.1 255.255.255.0\n")
+    time.sleep(2)
+    connection.send("no shut\n")
+    connection.send("end\n")
+    print("ca fonctionne")
+    time.sleep(2)
+
