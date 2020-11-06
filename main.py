d3999aa4bdfa42b40a1f8dd9acd6b667d218a7f1
Vincent33560 <qwezedits@hotmail.fr>
2020-11-06 22:55:07 +0100

    amelio avec except

--- main.py
+++ main.py
@@ -2,22 +2,46 @@ import paramiko
 import time
 import getpass
 
+
+
 ip = input("Entrez l'ip cible :")
 username = input("Entrez hostname :")
 password = input("Entrez le mot de passe :")
 
-SESSION = paramiko.SSHClient()
-SESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-SESSION.connect(ip,port=22,
+try:
+
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        ssh.connect(ip,port=22,
                 username=username,
                 password=password,
                 look_for_keys=False,
                 allow_agent=False)
+        print("Connection réussie !")
 
-DEVICE_ACCESS = SESSION.invoke_session()
-DEVICE_ACCESS.send(b'sh run')
-time.sleep(2)
-output = DEVICE_ACCESS.recv(65000)
-print(output.decode('ascii'))
+        connection = ssh.invoke_session()
+        connection.send("enable\n")
+        time.sleep(.5)
+        connection.send("vdcvdc\n")
+        time.sleep(2)
+        connection.send("show ip int brief\n")
+        time.sleep(2)
 
-SESSION.close
+        router_output = connection.recv(65535)
+        time.sleep(.5)
+        print("\n\n")
+        print(str(router_output) + "\n")
+        time.sleep(.5)
+
+        connection.send("end\n")
+
+    except paramiko.AuthenticationException:
+        print("Mot de passe incorrect" + password)
+
+    except socket.erro:
+        print("Erreur de socket")
+
+except:
+    print("Quelque chose ne va pas")
