6d0c1a48f4e3980c9c54d095db2ec827ac5818fa
Vincent33560 <qwezedits@hotmail.fr>
2020-11-06 22:29:55 +0100

    test avec ssh2

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ ip = input("Entrez l'ip cible :")
 username = input("Entrez hostname :")
 password = input("Entrez le mot de passe :")
 
-remote_con_pre = paramiko.SSHClient()
-remote_con_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-remote_con_pre.connect(ip,port=22,
+SESSION = paramiko.SSHClient()
+SESSION.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+SESSION.connect(ip,port=22,
                 username=username,
                 password=password,
                 look_for_keys=False,
                 allow_agent=False)
 
-remote_con = remote_con_pre.invoke_shell()
-remote_con.send(b'sh ip int br\n')
-output = remote_con.recv(65535)
-time.sleep(5)
-print output
+DEVICE_ACCESS = SESSION.invoke_session()
+DEVICE_ACCESS.send(b'sh run')
+time.sleep(2)
+output = DEVICE_ACCESS.recv(65000)
+print(output.decode('ascii'))
+
+SESSION.close
