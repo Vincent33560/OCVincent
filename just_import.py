372f62e4ccd2c90066c1a3ece4aba195ea2493c5
Vincent33560 <qwezedits@hotmail.fr>
2020-11-09 00:16:15 +0100

    Merge branch 'master' of https://github.com/Vincent33560/OCVincent
    # Conflicts:
    #	test.py

--- just_import.py
+++ just_import.py
@@ -1,8 +1,12 @@
-from test import *
+from test import run_command_on_device as SSH
+from test_import import exemple
 
 
-SSH = Connect_ssh()
-#lancement_fin()
+blabla = exemple()
+SSH()
+
+# SSH = Connect_ssh()
+# lancement_fin()
 # run_command_on_device(router_ip, router_username, router_password)
 
 print("ca fonctionne")
--- test.py
+++ test.py
@@ -6,53 +6,48 @@ router_ip = input("Entrez l'adresse IP cible : ")
 router_username = input("Entrez le username : ")
 router_password = input("Entrez le mot de passe : ")
 
-class Connect_ssh:
-
-    def run_command_on_device(router_ip, username, password):
-        """ Connect to a device, run a command, and return the output."""
-
+def run_command_on_device(router_ip, username, password):
+    """ Connect to a device, run a command, and return the output."""
+    try:
         ssh = paramiko.SSHClient()
         # Add SSH host key when missing.
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         # Load SSH host keys.
         ssh.load_system_host_keys()
 
-        total_attempts = 3
-        for attempt in range(total_attempts):
-            try:
-                print("Attempt to connect: %s" % attempt)
-                # Connect to router using username/password authentication.
-                ssh.connect(router_ip,
-                            username=router_username,
-                            password=router_password,
-                            look_for_keys=False)
-                # Run command.
-                DEVICE_ACCESS = ssh.invoke_shell()
-                DEVICE_ACCESS.send(b"en\n")
-                DEVICE_ACCESS.send(b"vdcvdc\n")
-                DEVICE_ACCESS.send(b"conf t\n")
-                DEVICE_ACCESS.send(b"int g0/1\n")
-                DEVICE_ACCESS.send(b"ip address 192.168.0.10 255.255.255.0\n")
-                DEVICE_ACCESS.send(b"no shut\n")
-                DEVICE_ACCESS.send(b"end\n")
-                DEVICE_ACCESS.send(b"sh ip int br\n")
+        try:
+            # Connect to router using username/password authentication.
+            ssh.connect(router_ip,
+                        username=router_username,
+                        password=router_password,
+                        look_for_keys=False)
+            print("Connexion réussie")
 
-                time.sleep(2)
-                print("test2")
+            # Run command.
+            DEVICE_ACCESS = ssh.invoke_shell()
+            DEVICE_ACCESS.send(b"en\n")
+            DEVICE_ACCESS.send(b"vdcvdc\n")
+            DEVICE_ACCESS.send(b"conf t\n")
+            DEVICE_ACCESS.send(b"int g0/1\n")
+            DEVICE_ACCESS.send(b"ip address 192.168.0.10 255.255.255.0\n")
+            DEVICE_ACCESS.send(b"no shut\n")
+            DEVICE_ACCESS.send(b"end\n")
+            DEVICE_ACCESS.send(b"sh ip int br\n")
 
-                # Read output from command.
-                output = DEVICE_ACCESS.recv(65000)
-                print(output.decode('ascii'))
-                # Close connection.
-                ssh.close()
+            time.sleep(2)
+            print("test2")
 
-            except Exception as error_message:
-                print("Unable to connect")
-                print(error_message)
+            # Read output from command.
+            output = DEVICE_ACCESS.recv(65000)
+            print(output.decode('ascii'))
+            # Close connection.
+            ssh.close()
 
-    run_command_on_device(router_ip, router_username, router_password)
+        except paramiko.AuthenticationException:
+            print("Incorrect password: ")
 
 
+    except:
+        print("Quelque chose ne vas pas")
 
-
-
+#run_command_on_device(router_ip, router_username, router_password)
