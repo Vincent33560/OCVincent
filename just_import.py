a2edf772d10d280f32c454e7f845214c20b8991a
Vincent LINUX <qwezedits@hotmail.fr>
2020-11-09 00:26:31 +0100

    modif

--- just_import.py
+++ just_import.py
@@ -1,11 +1,12 @@
 from test import run_command_on_device as SSH
-from test_import import exemple
 
 
-blabla = exemple()
-SSH()
 
-# SSH = Connect_ssh()
+
+
+bb = SSH()
+
+
 # lancement_fin()
 # run_command_on_device(router_ip, router_username, router_password)
 
