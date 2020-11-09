a237982aabc59b2b267bcfdcf67e03266af4e563
Vincent33560 <qwezedits@hotmail.fr>
2020-11-09 02:43:16 +0100

    Merge branch 'master' of https://github.com/Vincent33560/OCVincent
    # Conflicts:
    #	test.py

--- test4.py
+++ test4.py
@@ -4,39 +4,39 @@ import time
 IP = input("Entrez l'adresse IP cible : ")
 Username = input("Entrez le username : ")
 Password = input("Entrez le mot de passe : ")
+class Test:
+    def __init__(self, IP="", username="", password=""):
+        self.ip = IP,
+        self.username = username,
+        self.password = password,
 
-def __init__(self, IP="", username="", password=""):
-    self.ip = IP,
-    self.username = username,
-    self.password = password,
 
+        ssh = paramiko.SSHClient()
+        # Add SSH host key when missing.
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        # Load SSH host keys.
+        ssh.load_system_host_keys()
+        ssh.connect(IP,
+                    username=Username,
+                    password=Password,
+                    look_for_keys=False)
 
-    ssh = paramiko.SSHClient()
-    # Add SSH host key when missing.
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # Load SSH host keys.
-    ssh.load_system_host_keys()
-    ssh.connect(IP,
-                username=Username,
-                password=Password,
-                look_for_keys=False)
+    def Interfaces(self):
+        ssh = paramiko.SSHClient()
 
-def Interfaces():
-    ssh = paramiko.SSHClient()
+        DEVICE_ACCESS = ssh.invoke_shell()
+        DEVICE_ACCESS.send(b"en\n")
+        DEVICE_ACCESS.send(b"vdcvdc\n")
+        DEVICE_ACCESS.send(b"conf t\n")
+        DEVICE_ACCESS.send(b"int g0/2\n")
+        DEVICE_ACCESS.send(b"ip address 192.168.21.25 255.255.255.0\n")
+        DEVICE_ACCESS.send(b"no shut\n")
+        DEVICE_ACCESS.send(b"end\n")
+        DEVICE_ACCESS.send(b"sh ip int br\n")
+        time.sleep(1)
 
-    DEVICE_ACCESS = ssh.invoke_shell()
-    DEVICE_ACCESS.send(b"en\n")
-    DEVICE_ACCESS.send(b"vdcvdc\n")
-    DEVICE_ACCESS.send(b"conf t\n")
-    DEVICE_ACCESS.send(b"int g0/2\n")
-    DEVICE_ACCESS.send(b"ip address 192.168.21.25 255.255.255.0\n")
-    DEVICE_ACCESS.send(b"no shut\n")
-    DEVICE_ACCESS.send(b"end\n")
-    DEVICE_ACCESS.send(b"sh ip int br\n")
-    time.sleep(1)
+        # Read output from command.
+        output = DEVICE_ACCESS.recv(65000)
+        print(output.decode('ascii'))
 
-    # Read output from command.
-    output = DEVICE_ACCESS.recv(65000)
-    print(output.decode('ascii'))
-
-Interfaces(IP, Username,Password)
+blabla = Test(IP, Username,Password)
