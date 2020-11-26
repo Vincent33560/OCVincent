42347dc9590ba3a9544fab6147f462293313daf2
Vincent33560 <qwezedits@hotmail.fr>
2020-11-26 23:36:54 +0100

    ajout utils.py

--- main.py
+++ main.py
@@ -7,63 +7,7 @@ __date__ = "24/11/2020"
 import paramiko
 import time
 import logging
-
-
-def send(shell, command):
-    """
-    Function to send specific commands
-    """
-
-    shell.send("\n")
-    shell.send(str(command) + "\n")
-
-    time.sleep(.5)
-
-    output = shell.recv(65000)
-    print(output.decode('ascii'))
-
-
-def main():
-    """
-    Main function that initializes the ssh connection by asking for the targeted ip address,
-    user and password
-    """
-
-    logging.info('Connexion SSH')
-
-    router_ip = input("Entrez l'adresse IP cible : ")
-    router_username = input("Entrez le username : ")
-    router_password = input("Entrez le mot de passe : ")
-
-    try:
-        ssh_pre = paramiko.SSHClient()
-        ssh_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        ssh_pre.load_system_host_keys()
-
-        try:
-
-            # Connect to router using username/password authentication.
-            ssh_pre.connect(router_ip,
-                            username=router_username,
-                            password=router_password,
-                            look_for_keys=False)
-            print("Connexion réussie")
-            shell = ssh_pre.invoke_shell()
-            output = shell.recv(65000)
-            print(output.decode('ascii'))
-
-            send(shell, "enable")
-            send(shell, "vdcvdc\n")
-
-        except paramiko.AuthenticationException:
-            print("Mot de passe incorrect : ")
-            logging.error("Mauvais identifiant !")
-    except:
-        print("Quelque chose ne vas pas")
-        logging.error("Problème de connexion")
-    mainMenu(shell)
-
+from utils import *
 
 def mainMenu(shell):
     """
@@ -394,4 +338,5 @@ def saveLoad(shell):
 
 
 if __name__ == '__main__':
-    main()
+    shell = getSSHConnection()
+    mainMenu(shell)
---
+++ utils.py
@@ -0,0 +1,58 @@
+import time
+import logging
+import paramiko
+
+def send(shell, command):
+    """
+    Function to send specific commands
+    """
+
+    shell.send("\n")
+    shell.send(str(command) + "\n")
+
+    time.sleep(.5)
+
+    output = shell.recv(65000)
+    print(output.decode('ascii'))
+
+def getSSHConnection():
+    """
+    Main function that initializes the ssh connection by asking for the targeted ip address,
+    user and password
+    """
+
+    logging.info('Connexion SSH')
+
+    router_ip = input("Entrez l'adresse IP cible : ")
+    router_username = input("Entrez le username : ")
+    router_password = input("Entrez le mot de passe : ")
+
+    try:
+        ssh_pre = paramiko.SSHClient()
+        ssh_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ssh_pre.load_system_host_keys()
+
+        try:
+
+            # Connect to router using username/password authentication.
+            ssh_pre.connect(router_ip,
+                            username=router_username,
+                            password=router_password,
+                            look_for_keys=False)
+            print("Connexion réussie")
+            shell = ssh_pre.invoke_shell()
+            output = shell.recv(65000)
+            print(output.decode('ascii'))
+
+            send(shell, "enable")
+            send(shell, "vdcvdc\n")
+
+        except paramiko.AuthenticationException:
+            print("Mot de passe incorrect : ")
+            logging.error("Mauvais identifiant !")
+    except:
+        print("Quelque chose ne vas pas")
+        logging.error("Problème de connexion")
+    return(shell)
+
